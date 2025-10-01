450b7def46f65c51c1e0fa0cd6e0221ba4344daa
Sang <sang@miro.com>
2025-10-01 22:24:51 +0200

    udpate

--- src/allocator.c
+++ src/allocator.c
@@ -1,9 +1,15 @@
 #include "bamboo-private.h"
+#include <string.h>
+
+void bamboo_allocator_reset(struct bamboo_allocator* allocator) {
+    allocator->cursor = 0;
+    memset(allocator->data, 0, allocator->size);
+}
 
 void bamboo_allocator_init(struct bamboo_allocator* allocator, void* data, size_t size) {
     allocator->data = data;
     allocator->size = size;
-    allocator->cursor = 0;
+    bamboo_allocator_reset(allocator);
 }
 
 void* bamboo_allocator_alloc(struct bamboo_allocator* allocator, size_t size) {
@@ -13,10 +19,6 @@ void* bamboo_allocator_alloc(struct bamboo_allocator* allocator, size_t size) {
     void* ptr = (char*)allocator->data + allocator->cursor;
     allocator->cursor += size;
     return ptr;
-}
-
-void bamboo_allocator_reset(struct bamboo_allocator* allocator) {
-    allocator->cursor = 0;
 }
 
 size_t bamboo_allocator_remaining(const struct bamboo_allocator* allocator) {
