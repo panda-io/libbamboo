ddeca91c51250822120f6dc065df30f413ba37f3
Sang <sang@miro.com>
2025-10-01 23:29:58 +0200

    update

--- include/bamboo.h
+++ include/bamboo.h
@@ -66,7 +66,32 @@ bamboo_result_t bamboo_remove_at(struct bamboo_list* list, size_t index);
 
 /* text declarations */
 
+struct bamboo_text {
+    char* data;
+    size_t length;
+};
 
+struct bamboo_text* bamboo_text_create(struct bamboo_allocator* allocator, const char* str, size_t length);
+
+bamboo_result_t bamboo_text_copy_to_c_str(const struct bamboo_text* text, char* out, size_t out_size);
+
+/* string declarations */
+
+struct bamboo_string {
+    char* data;
+    size_t length;
+    size_t capacity;
+};
+
+struct bamboo_string* bamboo_string_create(struct bamboo_allocator* allocator, size_t capacity);
+
+bamboo_result_t bamboo_string_append_text(struct bamboo_string* string, const struct bamboo_text* text);
+
+bamboo_result_t bamboo_string_append_c_str(struct bamboo_string* string, const char* str);
+
+bamboo_result_t bamboo_string_copy_to_c_str(const struct bamboo_string* string, char* out, size_t out_size);
+
+void bamboo_string_clear(struct bamboo_string* string);
 
 #ifdef __cplusplus
 }
