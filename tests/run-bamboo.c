3584bb17819cc2e46aed408592b63121e9cc1301
Sang <sang@miro.com>
2025-10-02 18:24:27 +0200

    update

---
+++ tests/run-bamboo.c
@@ -0,0 +1,29 @@
+#include <bamboo.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+struct struct_with_string {
+    struct bamboo_string str;
+};
+
+int main() {
+    char buffer[1024];
+    struct bamboo_allocator allocator;
+    bamboo_allocator_init(&allocator, buffer, sizeof(buffer));
+
+    /* test string in a struct */
+    struct struct_with_string* s = bamboo_allocator_alloc(&allocator, sizeof(struct struct_with_string));
+    assert(s != NULL);
+    bamboo_string_init(&s->str, &allocator, 10);
+    assert(s->str.length == 0);
+    assert(s->str.capacity == 10);
+    assert(bamboo_string_append_c_str(&s->str, "Hello") == BAMBOO_SUCCESS);
+    assert(s->str.length == 5);
+    assert(s->str.capacity == 10);
+    assert(strncmp(s->str.data, "Hello", s->str.length) == 0);
+    assert(bamboo_string_append_c_str(&s->str, " World") == BAMBOO_OUT_OF_CAPACITY);
+    assert(bamboo_allocator_remaining(&allocator) == sizeof(buffer) - sizeof(struct struct_with_string) - 10);
+
+    return 0;
+}
